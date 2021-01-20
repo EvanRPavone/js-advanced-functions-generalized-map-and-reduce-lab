600a024d7e64b5c2e9e7907d8a327b90358ff760
EvanRPavone <evanrpavone@gmail.com>
2021-01-20 10:22:59 -0500

    commit tree 506f4df67382176db9bee31e413f0bb6de9d4394

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+function map(srcArray, fn) {
+    let newArr = [];
+    for (let i = 0; i < srcArray.length; i++) {
+        const element = fn(srcArray[i])
+        newArr.push(element)
+    }
+    return newArr
+}
+
+function reduce(srcArray, fn, start = 0){
+    let t = (!!start ? start : srcArray[0])
+    let i = (!!start ? 0 : 1)
+
+    for (;i < srcArray.length; i++) {
+        t = fn(srcArray[i], t)
+    }
+    return t
+}
